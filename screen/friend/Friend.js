a94ca22c54eb9c9096873dae5620f0715f89d6bf
김지섭 <cocacola158500@gmail.com>
2023-02-27 00:14:48 +0900

    feat/myprofile

--- screen/friend/Friend.js
+++ screen/friend/Friend.js
@@ -1,32 +1,39 @@
 // react
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, Image } from "react-native";
 
 // custom
 import styles from "./Style";
 
 const Friend = () => {
+  const [profileName, setProfileName] = useState("이름");
+  const [profileMessage, setProfileMessage] = useState("프로필메세지");
   return (
-    <View style={{ flex: 1, backgroundColor: "white" }}>
+    <View style={styles.container}>
       <View style={{ flexDirection: "row" }}>
-        <Image source={require("@public/image/user_01.png")} style={{ width: 30, height: 30 }} />
-        <View>
-          <Text>내 정보</Text>
-          <Text>상태 메시지</Text>
+        <Image
+          source={require("@public/image/user_01.png")}
+          style={styles.profileImg}
+        />
+        <View style={styles.profileInclude}>
+          <Text style={styles.profileNameStyle}>{profileName}</Text>
+          <Text style={styles.profileMessageStyle}>{profileMessage}</Text>
         </View>
-        <Text stlye={{ marginLeft: 30 }}>인스턴스 메시지</Text>
+        <View style={styles.InstanceBox}>
+          <Text style={styles.InstanceMessage}>인스턴스 메시지</Text>
+        </View>
       </View>
       <View style={styles.settingLineView} />
       <View>
-        <Text>즐겨 찾기</Text>
+        <Text style={styles.contentText}>즐겨 찾기</Text>
       </View>
       <View style={styles.settingLineView} />
       <View>
-        <Text>생일자</Text>
+        <Text style={styles.contentText}>생일자</Text>
       </View>
       <View style={styles.settingLineView} />
       <View>
-        <Text>친구목록</Text>
+        <Text style={styles.contentText}>친구목록</Text>
       </View>
     </View>
   );
--- screen/friend/Style.js
+++ screen/friend/Style.js
@@ -1,6 +1,45 @@
 import { StyleSheet } from "react-native";
-import { windowHeight } from "@enum/size";
+import { windowWidth, windowHeight } from "@enum/size";
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  profileImg: {
+    marginLeft: windowWidth * 10,
+    width: windowWidth * 50,
+    height: windowWidth * 50,
+  },
+  profileInclude: {
+    justifyContent: "center",
+    alignSelf: "center",
+  },
+  profileNameStyle: {
+    marginTop: windowHeight * 5,
+    marginLeft: windowWidth * 5,
+    marginBottom: windowHeight * 5,
+    fontSize: windowWidth * 15,
+    fontWeight: "500",
+  },
+  profileMessageStyle: {
+    marginLeft: windowWidth * 5,
+    fontSize: windowWidth * 12,
+  },
+  InstanceBox: {
+    flex: 1,
+    marginVertical: windowHeight * 7,
+    marginLeft: windowWidth * 50,
+    marginRight: windowWidth * 10,
+    backgroundColor: "#cccccc",
+    justifyContent: "center",
+  },
+  InstanceMessage: {
+    marginLeft: windowWidth * 10,
+    fontSize: windowWidth * 12,
+  },
+  contentText: {
+    marginLeft: windowWidth * 10,
+  },
   settingLineView: {
     width: "100%",
     height: windowHeight * 5,
