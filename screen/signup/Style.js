553b9e8a14bbd6346396342d640e2641d78ba50d
김지섭 <cocacola158500@gmail.com>
2023-04-25 00:42:06 +0900

    회원가입폼 구현 1.비밀번호 정규식적용 2.가입하기 버튼이동

--- screen/signup/Style.js
+++ screen/signup/Style.js
@@ -1,7 +1,16 @@
+import { windowHeight } from '@enum/size';
 import { StyleSheet } from 'react-native';
-
 const styles = StyleSheet.create({
-
+  checkText: {
+    fontSize: 15,
+    marginBottom: windowHeight * 15,
+  },
+  lineView: {
+    width: '90%',
+    height: windowHeight * 1,
+    backgroundColor: 'gray',
+    marginBottom: windowHeight * 15,
+  },
 });
 
 export default styles;
