97e61af38f71d9654d3cf5161a8009d5f2d83149
김지섭 <cocacola158500@gmail.com>
2023-04-28 02:48:58 +0900

    회원가입 연결 1. 이메일 사용가능여부  -중복검사  -이메일형식검사 2.비밀번호 확인  -비밀번호 형식검사(7자이상+특수문자1개이상포함)  -비밀번호 확인검사(1차비밀번호와 동일한지 여부) 3.휴대폰인증  -휴대폰자릿수검사(11자리검사)  -휴대폰문자인증연결 4.차후수정사항  -인증번호확인

---
+++ enum/server.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://avalon-chat-202n-237c5z6xwa-du.a.run.app/v1'; //SERVER URL
+export const SIGNUP = '/signup';
+export const SIGNUP_EMAIL_DUPULICATED = '/signup/email/duplicated';
+export const SIGNUP_PHONE_SEND = '/signup/phonenumber/authenticate/send';
+export const SIGNUP_PHONE_CHECK = '/signup/phonenumber/authenticate/check';
