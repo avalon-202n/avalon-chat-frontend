72432d07c2a05f3cbba0946782b0e532c2269dc4
김지섭 <cocacola158500@gmail.com>
2023-04-28 03:38:27 +0900

    스타일 추가 변수명 수정

--- screen/signup/Style.js
+++ screen/signup/Style.js
@@ -1,8 +1,31 @@
-import { windowHeight } from '@enum/size';
+import { windowHeight, windowWidth } from '@enum/size';
 import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  inputContainer: {
+    flex: 3,
+    flexDirection: 'row',
+  },
+  SignupBtnContainer: {
+    flex: 0.3,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'green',
+  },
+  SignupBtn: {
+    flex: 1,
+    backgroundColor: 'pink',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  SignupText: {
+    fontSize: windowWidth * 20,
+  },
   checkText: {
-    fontSize: 15,
+    fontSize: windowWidth * 15,
     marginBottom: windowHeight * 15,
   },
   lineView: {
