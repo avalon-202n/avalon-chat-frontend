efa4a4cbf7347af29f34bd4c1321eee01e7a10aa
김지섭 <cocacola158500@gmail.com>
2023-05-23 14:14:37 +0900

    앱 초기화면 splash 화면 1.5초 변경 (이미지 스타일 미지정)

---
+++ screen/splash/Style.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+const styles = StyleSheet.create({
+  //Splash
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignContent: 'center',
+  },
+  mainImage: {},
+});
+
+export default styles;
